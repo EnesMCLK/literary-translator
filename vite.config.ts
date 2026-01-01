b8542f6479e35d308f46cd510506836b343d44d6
EnesMCLK <40140673+EnesMCLK@users.noreply.github.com>
2026-01-01 18:53:33 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,11 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  root: './literary-translator/',          // Dosyalar kök dizinde
   base: '/literary-translator/',           // Site kök dizinden çalışacak
   build: {
     outDir: 'dist',    // Çıktı klasörü dist
     emptyOutDir: true, // Her seferinde temizle
   }
 })
+
 
